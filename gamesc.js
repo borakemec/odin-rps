d7b205a89d27ba569af3805bbadbe1734b8204f5
borakemec <69791017+borakemec@users.noreply.github.com>
2024-07-18 22:59:32 -0400

    Update gamesc.js

--- gamesc.js
+++ gamesc.js
@@ -1,75 +1,106 @@
 const rpsArray = ['rock', 'paper', 'scissors'];
 let humanScore = 0;
 let computerScore = 0;
+let firstGame = true;
+let bigText = document.querySelector('h1');
+let scoreText;
+let restartBtn;
+let secondline = document.querySelector('.secondline');
 
 function getComputerChoice() {
     return(rpsArray[Math.floor(Math.random()*rpsArray.length)]);
 }
 
-function getHumanChoice() {
-    while(true) {
-        let response = prompt('Rock, Paper or Scissors');
-        if(rpsArray.includes(response)) {
-            return response;
-        }
+function playRound(humanChoice) {
+    let computerChoice = getComputerChoice();
+    if(firstGame) {
+        firstGame = false;
+        scoreText = document.createElement('p');
+        scoreText.innerText = `${humanScore} - ${computerScore}`;
+        scoreText.style.color = "white";
+        scoreText.style.fontFamily = "Arial";
+        scoreText.style.fontSize = "30px";
+        scoreText.style.marginLeft = '130px';
+        secondline.appendChild(scoreText);
+        document.body.style.gap = "49px";
+        restartBtn = document.createElement('input');
+        restartBtn.setAttribute('type', 'button');
+        restartBtn.style.width = '100px';
+        restartBtn.style.height = '33px';
+        restartBtn.setAttribute('value', 'Restart');
+        restartBtn.style.fontSize = "15px";
+        secondline.appendChild(restartBtn);
+        restartBtn.addEventListener('click', restartGame);
     }
-}
 
-function playRound() {
-    let humanSelect = getHumanChoice();
-    let computerSelect = getComputerChoice();
-
-    if(humanSelect.toLowerCase() === computerSelect) {
-        return "Draw";
+    if (humanChoice === computerChoice) {
+        bigText.innerText = `Draw: ${humanChoice} - ${computerChoice}`;
     }
 
-    else if(humanSelect.toLowerCase() === 'rock') {
-        if(computerSelect === 'paper') {
+    else if(humanChoice === 'rock') {
+        if (computerChoice === 'paper') {
             computerScore++;
-            return "Computer Wins The Round";
+            bigText.innerText = `Computer Wins: ${computerChoice} - ${humanChoice}`;
         }
         else {
             humanScore++;
-            return "You Win The Round";
+            bigText.innerText = `You Win: ${humanChoice} - ${computerChoice}`;
         }
     }
 
-    else if(humanSelect.toLowerCase() === 'scissors') {
-        if(computerSelect === 'paper') {
-            humanScore++;
-            return "You Win The Round";
+    else if(humanChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            computerScore++;
+            bigText.innerText = `Computer Wins: ${computerChoice} - ${humanChoice}`;
         }
         else {
-            computerScore++;
-            return "Computer Wins The Round";
+            humanScore++;
+            bigText.innerText = `You Win: ${humanChoice} - ${computerChoice}`;
         }
     }
 
     else {
-        if(computerSelect === 'rock') {
-            humanScore++;
-            return "You Win The Round";
+        if (computerChoice === 'rock') {
+            computerScore++;
+            bigText.innerText = `Computer Wins: ${computerChoice} - ${humanChoice}`;
         }
         else {
-            computerScore++;
-            return "Computer Wins The Round";
+            humanScore++;
+            bigText.innerText = `You Win: ${humanChoice} - ${computerChoice}`;
         }
     }
+
+    scoreText.innerText = `${humanScore} - ${computerScore}`;
+
+    if(humanScore === 5) {
+        bigText.innerText = "You Win The Game!";
+        rockbtn.disabled = true;
+        paperbtn.disabled = true;
+        scissorsbtn.disabled = true;
+    }
+
+    if(computerScore === 5) {
+        bigText.innerText = "Computer Wins The Game!";
+        rockbtn.disabled = true;
+        paperbtn.disabled = true;
+        scissorsbtn.disabled = true;
+    }
 }
 
-function playGame() {
-    for(let i = 0; i < 5; i++) {
-        console.log(playRound());
-    }
-    if(humanScore > computerScore) {
-        console.log(`You win the game ${humanScore} - ${computerScore}!`);
-    }
-    else if(computerScore > humanScore) {
-        console.log(`Computer wins the game ${computerScore} - ${humanScore}!`);
-    }
-    else {
-        console.log(`You draw with the computer ${computerScore} - ${humanScore}!`);
-    }
+function restartGame() {
+    computerScore = 0;
+    humanScore = 0;
+    scoreText.innerText = `${humanScore} - ${computerScore}`;
+    rockbtn.disabled = false;
+    paperbtn.disabled = false;
+    scissorsbtn.disabled = false;
+    bigText.innerText = "Pick One";
 }
 
-document.getElementById('playbtn').onclick = playGame;
+let rockbtn = document.querySelector('.rockbtn');
+let paperbtn = document.querySelector('.paperbtn');
+let scissorsbtn = document.querySelector('.scissorsbtn');
+
+rockbtn.addEventListener('click', () => playRound('rock'));
+paperbtn.addEventListener('click', () => playRound('paper'));
+scissorsbtn.addEventListener('click', () => playRound('scissors'));
